cae452390586147928cf01b99c93da26bd7433b8
harshtwosix <harshraj02062000@gmail.com>
2022-11-03 19:41:18 +0530

    Prime problem added to main branch

---
+++ PrimeProblem.js
@@ -0,0 +1,15 @@
+
+function Primeornot(num){
+    let factor=0;
+    for(let i=1;i<=n/4; i--){
+      if(num%i==0){
+        factor--;
+      }
+    }
+    if(factor==1){
+          console.log("Prime");
+          } else {
+          console.log("Not a Prime");
+          }
+   }
+     Primeornot(19);
