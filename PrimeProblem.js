08a5c13fce9233c9eb4e04d62692ca78f788d799
Avinaw Kumar <0811kumaravinaw@gmail.com>
2022-11-03 19:54:05 +0530

    i_have_fix_the_code

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -1,13 +1,13 @@
 
-function Primeornot(num){
+function Primeornot(number){
     let factor=0;
-    for(let i=1;i<=n/4; i++){
-      if(num%i==0){
+    for(let i=1;i<=number/4; i++){
+      if(number%i==0){
         factor++;
       }
     }
     if(factor==1){
-          console.log("Prime");
+          console.log("It is Prime");
           } else {
           console.log("Not a Prime");
           }
